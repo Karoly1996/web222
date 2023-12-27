a5db6e9530579aa495fe7108c54fc76f84b66e91
Karoly <120995235+Karoly1996@users.noreply.github.com>
2023-12-26 23:16:43 -0500

    Add files via upload

---
+++ assignment4/js/artists.js
@@ -0,0 +1,39 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+// <!-- Karoly Nemeth | 021949144 | knemeth@myseneca.ca -->
+
+window.artists = [
+  //Artist 1
+  {
+    artistID: "AID-1",
+    name: "Martin Garrix",
+    urls: [
+      { url: "https://martingarrix.com/", name: "Website" },
+      { url: "https://www.instagram.com/martingarrix/?hl=en", name: "Instagram" }
+    ]
+  },
+
+  // Artist 2
+  {
+    artistID: "AID-2",
+    name: "Tiesto",
+    urls: [
+      { url: "https://www.tiesto.com/", name: "Website" },
+      { url: "https://www.instagram.com/tiesto/?hl=en", name: "Instagram" }
+    ]
+  },
+
+  // Artist 3
+  {
+    artistID: "AID-3",
+    name: "Avicii",
+    urls: [
+      { url: "https://avicii.com/", name: "Website" },
+      { url: "https://www.instagram.com/avicii/?hl=en", name: "Instagram" }
+    ]
+  }
+];
